5926f1d0b0b97916a7c63c15bd3b5ed11c5a5802
matsen17 <matas.senkus17@gmail.com>
2020-05-07 00:01:23 +0300

    Added flashlight

---
+++ Evadere-game/Evadere/Assets/Scripts/FlashlightTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashlightTrigger : MonoBehaviour
+{
+    public GameObject flashlight;
+    bool fEnabled = false;
+
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            fEnabled = !fEnabled;
+        }
+
+        if (fEnabled)
+            flashlight.SetActive(true);
+        else
+            flashlight.SetActive(false);
+    }
+}
